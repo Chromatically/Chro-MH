8f89e18c3d900590a6562779f62f79bffbf8c011
Chromatically <85063206+Chromatically@users.noreply.github.com>
2022-10-06 13:16:01 +0800

    Create MH - Uncaught Mouse Stats.user.js

---
+++ MH - Uncaught Mouse Stats.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         MH - Uncaught Mouse Stats
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.mousehuntgame.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mousehuntgame.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
