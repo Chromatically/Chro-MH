1f99c4ae67dabc2c2d7deff89c2b8add0ec90ee2
Chromatically <85063206+Chromatically@users.noreply.github.com>
2022-10-06 13:35:18 +0800

    Update MH - Uncaught Mouse Stats.user.js

--- MH - Uncaught Mouse Stats.user.js
+++ MH - Uncaught Mouse Stats.user.js
@@ -2,7 +2,7 @@
 // @name         MH - Uncaught Mouse Stats
 // @namespace    http://tampermonkey.net/
 // @version      0.11
-// @description  Displays any uncaught mice in a location
+// @description  Displays any uncaught mice in a location.
 // @author       MI
 // @match        https://www.mousehuntgame.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mousehuntgame.com
@@ -13,4 +13,5 @@
     'use strict';
 
     // Your code here...
+    let var = x
 })();
