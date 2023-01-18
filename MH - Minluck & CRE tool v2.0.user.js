5463f67ca1c56f2a4f64c5e721a2d14d3de31171
Neb <75031454+Neb-RS@users.noreply.github.com>
2023-01-18 15:22:47 +0100

    Update MH - Minluck & CRE tool v2.0.user.js
    Not today

--- MH - Minluck & CRE tool v2.0.user.js
+++ MH - Minluck & CRE tool v2.0.user.js
@@ -5,7 +5,7 @@
 // @match        https://www.mousehuntgame.com/*
 // @match        https://apps.facebook.com/mousehunt/*
 // @icon         https://www.google.com/s2/favicons?domain=mousehuntgame.com
-// @version      2.6.0
+// @version      2.6.1
 // @grant        none
 // @namespace https://greasyfork.org/users/748165
 // ==/UserScript==
