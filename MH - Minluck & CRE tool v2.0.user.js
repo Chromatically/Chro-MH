2275f6c8cd5945e03473809d07dd17a96b7d349c
Neb <75031454+Neb-RS@users.noreply.github.com>
2023-02-03 21:43:42 +0100

    Update MH - Minluck & CRE tool v2.0.user.js
    Revert LNY specific Dumpling Chef effs

--- MH - Minluck & CRE tool v2.0.user.js
+++ MH - Minluck & CRE tool v2.0.user.js
@@ -5,7 +5,7 @@
 // @match        https://www.mousehuntgame.com/*
 // @match        https://apps.facebook.com/mousehunt/*
 // @icon         https://www.google.com/s2/favicons?domain=mousehuntgame.com
-// @version      2.6.1
+// @version      2.6.2
 // @grant        none
 // @namespace https://greasyfork.org/users/748165
 // ==/UserScript==
@@ -1165,7 +1165,7 @@ var allMiceInfo = {
     },
     "Dumpling Chef": {
         "power": 2000,
-        "effs": [100,100,100,100,100,100,100,175,100,100],
+        "effs": 0,0,0,0,0,100,0,175,0,0,
     },
     "Dumpling Delivery": {
         "power": 2305,
